ae2438865d8ef938c75179fb97719fd1de3fe034
Bilal Öztürk <bilalozturk60@hotmail.com>
2021-01-22 21:55:32 +0300

    20 Ocak Ders Sonu

---
+++ 4GunOdev/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4GunOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Türkiye'de Bulunan Bazı İllerin Plaka Numaraları
+            
+            MyDictionary<string, int> sehir = new MyDictionary<string, int>();
+
+            sehir.Add("Adana", 01);
+            sehir.Add("Bursa", 16);
+            sehir.Add("Çorum", 19);
+            sehir.Add("Denizli", 20);
+            sehir.Add("Ankara", 06);
+            sehir.Add("İstanbul", 34);
+            sehir.Add("İzmir", 35);
+            sehir.Add("Tokat", 60);
+            
+
+            foreach (var city in sehir.Items)
+            {
+                Console.WriteLine("Şehir : " + city.Item1 + "\tPlaka Kodu : (" + city.Item2 + ")");
+            }
+
+            Console.WriteLine("Toplam Plaka Kodu ve İsmi Yazılan İl Sayısı " + sehir.Items.Length);
+        }
+
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler2 = new List<string> { "Bilal", "Meryem", "Hira Nur", "Ömer Asaf" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Hüsamettin");
+
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            
+        }
+    }
+}
